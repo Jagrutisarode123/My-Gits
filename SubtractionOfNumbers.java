921f016975dab4572bd1e37da8185952939019b1
sarodejagruti <sarodejagruti123@gmail.com>
2023-02-02 11:45:33 +0530

    My Java SUBTRACTION Program

---
+++ SubtractionOfNumbers.java
@@ -0,0 +1,21 @@
+package Jagruti;
+
+import java.util.Scanner;
+public class SubtractionOfNumbers {
+
+
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Hello! Subtraction 2 float numbers");
+			System.out.println("\n Enter first no :- ");
+			float no1 = sc.nextFloat();
+			System.out.println("\n Enter second no:- ");
+			float no2 = sc.nextFloat();
+			System.out.println("addition is "+no1+"-"+no2+" = "+ (no1-no2));
+		}
+
+	
+}
